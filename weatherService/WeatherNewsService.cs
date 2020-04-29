6f57017c82e7b55a30b846e90a3212d0edc8f041
이지남 <jn.lee@wconcept.co.kr>
2020-04-29 10:58:49 +0900

    QuerySelector 이용한 DOM 값 가져오기로 수정

--- weatherService/WeatherNewsService.cs
+++ weatherService/WeatherNewsService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Fizzler.Systems.HtmlAgilityPack;
 using HtmlAgilityPack;
 using weatherModel;
 
@@ -13,10 +14,16 @@ namespace weatherService
             
         public IEnumerable<WeatherNews> GetWeatherNews()
         {
+            return GetWeatherNewsUsingFizzler();
+        }
+
+        private IEnumerable<WeatherNews> GetWatherNeewsUsingAgility()
+        {
             var htmlWeb = new HtmlWeb();
             var webPage = htmlWeb.Load("https://search.naver.com/search.naver?where=video&ie=utf8&sort=rel&listmode=v&stype=&period=&playtime=&selected_cp=&nso=&x_video=&sm=tab_pge&query=날씨&start=1&offset=100");
             var target = webPage.GetElementbyId("main_pack").Descendants("dt");
-            var weatherNews = new List<WeatherNews>{ };
+
+            var weatherNews = new List<WeatherNews> { };
 
             foreach (var item in target)
             {
@@ -27,6 +34,34 @@ namespace weatherService
                 var title = item.Descendants("a").Select(s => s.GetAttributeValue("title", null)).FirstOrDefault();
                 var movieLink = item.Descendants("a").Select(s => s.GetAttributeValue("href", null)).FirstOrDefault();
 
+                weatherNews.Add(new WeatherNews
+                {
+                    Title = title,
+                    LinkUrl = movieLink,
+                    BroadcastingCompany = broadcastCompany,
+                    Thumbnail = thumbImage,
+                });
+            }
+
+            return weatherNews.AsEnumerable();
+        }
+
+        private IEnumerable<WeatherNews> GetWeatherNewsUsingFizzler()
+        {
+            var weatherNews = new List<WeatherNews> { };
+            var htmlWeb = new HtmlWeb();
+
+            var webPage = htmlWeb.Load("https://search.naver.com/search.naver?where=video&ie=utf8&sort=rel&listmode=v&stype=&period=&playtime=&selected_cp=&nso=&x_video=&sm=tab_pge&query=날씨&start=1&offset=100");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(webPage.DocumentNode.OuterHtml);
+
+            var e = doc.DocumentNode.QuerySelectorAll(".video_item");
+            foreach(var item in e.Elements())
+            {
+                var title = item.QuerySelector(".info_title").Descendants("a").Select(x => x.GetAttributeValue("title", null)).FirstOrDefault();
+                var movieLink = item.QuerySelector(".info_title").Descendants("a").Select(x => x.GetAttributeValue("href", null)).FirstOrDefault();
+                var broadcastCompany = item.QuerySelector(".info.cite").Descendants("a").Select(x => x.GetAttributeValue("title", null)).FirstOrDefault();
+                var thumbImage = item.QuerySelector(".video_thum").Descendants("img").Select(x => x.GetAttributeValue("src", null)).FirstOrDefault();
                 weatherNews.Add(new WeatherNews
                 {
                     Title = title,
